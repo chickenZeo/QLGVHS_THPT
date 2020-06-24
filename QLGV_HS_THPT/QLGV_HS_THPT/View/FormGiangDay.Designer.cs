efcd368186a6ab50da31f8ac29025a012e23f8d3
= <=>
2020-06-24 18:31:07 +0700

    Long commit form GiangDay voi sua xoa them

---
+++ QLGV_HS_THPT/QLGV_HS_THPT/View/FormGiangDay.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace QLGV_HS_THPT.View
+{
+    partial class FormGiangDay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Bt_Xoa = new System.Windows.Forms.Button();
+            this.bt_Sua = new System.Windows.Forms.Button();
+            this.bt_Them = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_MaLop = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dtime_NgayDay = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_MaGV = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtgr = new System.Windows.Forms.DataGridView();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgr)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Bt_Xoa
+            // 
+            this.Bt_Xoa.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.Bt_Xoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bt_Xoa.Location = new System.Drawing.Point(307, 249);
+            this.Bt_Xoa.Name = "Bt_Xoa";
+            this.Bt_Xoa.Size = new System.Drawing.Size(86, 27);
+            this.Bt_Xoa.TabIndex = 13;
+            this.Bt_Xoa.Text = "Xóa";
+            this.Bt_Xoa.UseVisualStyleBackColor = false;
+            this.Bt_Xoa.Click += new System.EventHandler(this.Bt_Xoa_Click);
+            // 
+            // bt_Sua
+            // 
+            this.bt_Sua.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.bt_Sua.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_Sua.Location = new System.Drawing.Point(164, 249);
+            this.bt_Sua.Name = "bt_Sua";
+            this.bt_Sua.Size = new System.Drawing.Size(86, 27);
+            this.bt_Sua.TabIndex = 12;
+            this.bt_Sua.Text = "Sửa";
+            this.bt_Sua.UseVisualStyleBackColor = false;
+            this.bt_Sua.Click += new System.EventHandler(this.bt_Sua_Click);
+            // 
+            // bt_Them
+            // 
+            this.bt_Them.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.bt_Them.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_Them.Location = new System.Drawing.Point(36, 249);
+            this.bt_Them.Name = "bt_Them";
+            this.bt_Them.Size = new System.Drawing.Size(86, 27);
+            this.bt_Them.TabIndex = 11;
+            this.bt_Them.Text = "Thêm";
+            this.bt_Them.UseVisualStyleBackColor = false;
+            this.bt_Them.Click += new System.EventHandler(this.bt_Them_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_MaLop);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.dtime_NgayDay);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txt_MaGV);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 282);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(757, 122);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin giảng dạy";
+            // 
+            // txt_MaLop
+            // 
+            this.txt_MaLop.Location = new System.Drawing.Point(111, 60);
+            this.txt_MaLop.Name = "txt_MaLop";
+            this.txt_MaLop.Size = new System.Drawing.Size(188, 22);
+            this.txt_MaLop.TabIndex = 3;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(5, 64);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 18);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "MaLop";
+            // 
+            // dtime_NgayDay
+            // 
+            this.dtime_NgayDay.CustomFormat = "yyyy/MM/dd";
+            this.dtime_NgayDay.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtime_NgayDay.Location = new System.Drawing.Point(495, 23);
+            this.dtime_NgayDay.Name = "dtime_NgayDay";
+            this.dtime_NgayDay.Size = new System.Drawing.Size(113, 22);
+            this.dtime_NgayDay.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(375, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "NgayDay";
+            // 
+            // txt_MaGV
+            // 
+            this.txt_MaGV.Location = new System.Drawing.Point(111, 23);
+            this.txt_MaGV.Name = "txt_MaGV";
+            this.txt_MaGV.Size = new System.Drawing.Size(188, 22);
+            this.txt_MaGV.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(5, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MaGV";
+            // 
+            // dtgr
+            // 
+            this.dtgr.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtgr.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgr.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dtgr.Location = new System.Drawing.Point(0, 0);
+            this.dtgr.Name = "dtgr";
+            this.dtgr.RowTemplate.Height = 24;
+            this.dtgr.Size = new System.Drawing.Size(783, 243);
+            this.dtgr.TabIndex = 9;
+            this.dtgr.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgr_CellContentClick);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(468, 253);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 18);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Tìm kiếm";
+            // 
+            // FormGiangDay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 412);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.Bt_Xoa);
+            this.Controls.Add(this.bt_Sua);
+            this.Controls.Add(this.bt_Them);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dtgr);
+            this.Name = "FormGiangDay";
+            this.Text = "FormGiangDay";
+            this.Load += new System.EventHandler(this.FormGiangDay_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgr)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Bt_Xoa;
+        private System.Windows.Forms.Button bt_Sua;
+        private System.Windows.Forms.Button bt_Them;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_MaLop;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DateTimePicker dtime_NgayDay;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_MaGV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dtgr;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ QLGV_HS_THPT/QLGV_HS_THPT/View/FormGiangDay.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLGV_HS_THPT.View
+{
+    public partial class FormGiangDay : Form
+    {
+        public FormGiangDay()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("server = DESKTOP-1HGH8ER\\SQLEXPRESS;database = QLGV_HS_THPT; integrated security = SSPI");
+        private void HienThi(string truyvan)
+        {
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter(truyvan, con); // lay du lieu vao da
+            DataTable tb = new DataTable();
+            da.Fill(tb); // do du lieu vao table
+            dtgr.DataSource = tb; // do table len view
+            con.Close();
+        }
+        private void FormGiangDay_Load(object sender, EventArgs e)
+        {
+            HienThi("select * from GiangDay");
+        }
+
+        private void dtgr_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txt_MaGV.Text = dtgr.CurrentRow.Cells[0].Value.ToString();
+            txt_MaLop.Text = dtgr.CurrentRow.Cells[1].Value.ToString();
+            dtime_NgayDay.Text = dtgr.CurrentRow.Cells[2].Value.ToString();
+
+        }
+
+        private void bt_Them_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cd = con.CreateCommand(); // Tu nguon cua sql tao 1 command
+            cd.CommandText = "insert into GiangDay values('" + txt_MaGV.Text + "',N'" + txt_MaLop.Text + "','" + dtime_NgayDay.Text + "' )";
+            cd.ExecuteNonQuery();
+            con.Close();
+            HienThi("select * from GiangDay");
+        }
+
+        private void bt_Sua_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cd = con.CreateCommand(); // Tu nguon cua sql tao 1 command
+            cd.CommandText = "update GiangDay set MaLop= N'" + txt_MaLop.Text + "', NgayDay = '" + dtime_NgayDay.Text +"' where MaGV ='" + txt_MaGV.Text + "'";
+            cd.ExecuteNonQuery();
+            con.Close();
+            HienThi("select * from GiangDay");
+        }
+
+        private void Bt_Xoa_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cd = con.CreateCommand();
+            cd.CommandText = "delete from GiangDay where MaGV = '" + txt_MaGV.Text + "'";
+            cd.ExecuteNonQuery();
+            con.Close();
+            HienThi("select * from GiangDay");
+        }
+    }
+}
